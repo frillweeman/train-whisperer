21236291404f8e7027499b88d3fdeeece766cace
Will Freeman <hohosanta@me.com>
2023-12-17 00:53:20 -0600

    connect to rabbitmq properly

--- webapp/src/routes/api/transcriptions/live/+server.ts
+++ webapp/src/routes/api/transcriptions/live/+server.ts
@@ -1,27 +1,50 @@
 import { RequestHandler } from '@sveltejs/kit';
+import amqp from "amqplib/callback_api";
+import { Transcription } from '../../../../types';
 
-// TODO: rabbitmq consumer needs to provide data to the SSE stream
+// TODO: make this less hacky
+let mqConnection: any = null;
+
+function streamSSEFromRabbitMQ(controller: ReadableStreamDefaultController) {
+  amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      const queue = 'transcribed'; // TODO: make this a configured value
+      channel.assertQueue(queue, {
+        durable: false
+      });
+      // need to encode transcription as a JSON string
+      channel.consume(queue, function(msg: string) {
+        const transcription: Transcription = JSON.parse(msg);
+        controller.enqueue(transcription);
+      }, {
+        noAck: true
+      });
+    });
+  });
+}
+
+function cleanUpRabbitMQ() {
+  if (mqConnection) {
+    mqConnection.close();
+    mqConnection = null;
+  }
+}
 
 export const GET: RequestHandler = async ({ url }) => {
   const stream = new ReadableStream({
-    start(controller) {
-      // You can enqueue multiple data asynchronously here.
-      const myData = ["abc", "def"]
-      myData.forEach(data => {
-          controller.enqueue(`data: ${data}\n\n`)
-      })
-      controller.close() 
-    },
-    cancel() {
-      // cancel your resources here
-    }
+    start: streamSSEFromRabbitMQ,
+    cancel: cleanUpRabbitMQ
   });
 
   return new Response(stream, {
     headers: {
-      // Denotes the response as SSE
       'Content-Type': 'text/event-stream', 
-      // Optional. Request the GET request not to be cached.
       'Cache-Control': 'no-cache', 
     }
   })
